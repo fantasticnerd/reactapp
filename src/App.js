87fb48f5aef2ae1e29d316b08f911af3ebe83860
fantasticnerd <71866267+fantasticnerd@users.noreply.github.com>
2021-07-08 13:22:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div>
+  <link rel="stylesheet" href="test.css" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+  <div className="wrapper">
+    <h1 className="title">Preet Jain</h1><h1>
+    </h1></div>
+  <div className="container">
+    <p className="info">Hello World!! <br />
+      My name is Preet Jain. I am a 21 years old aspiring<strong>Web Developer</strong>
+      I started my coding journey journey five years ago. The languages that I speak are <br />
+      HTML,CSS,JavaScript,Java,C++ and Python.
+    </p>
+  </div>
+  <div className="center">
+    <div id="social-test">
+      <ul className="social">
+        <li><i className="fa fa-facebook" aria-hidden="true" /></li>
+        <li><i className="fa fa-twitter" aria-hidden="true" />
+        </li>
+        <li><i className="fa fa-instagram" aria-hidden="true" /></li>
+        <li><i className="fa fa-reddit" aria-hidden="true" />
+        </li>
+        <li><i className="fa fa-github" aria-hidden="true" />
+        </li>
+\      </ul>
+    </div>
+  </div>
+</div>
+
+  );
+}
+
+export default App;
